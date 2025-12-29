53d9252846f83c843271bab9c4895090454b676a
ahmadwagiehlaw <AhmadWagieh@law.helwan.edu.eg>
2025-12-29 15:11:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   storageBucket: "ers-runners-app.firebasestorage.app",
   messagingSenderId: "493110452684",
   appId: "1:493110452684:web:db892ab6e6c88b3e6dbd69"
-};
+}; 
 
 if (!firebase.apps.length) { firebase.initializeApp(firebaseConfig); }
 const auth = firebase.auth();
