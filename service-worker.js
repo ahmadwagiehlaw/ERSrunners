1e767bacd74084b989ca526211a5a4c869ef1f1f
ahmadwagiehlaw <AhmadWagieh@law.helwan.edu.eg>
2025-12-30 19:10:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// ERS Runners - Service Worker (V1.123)
+// ERS Runners - Service Worker (V1.25)
 const CACHE_NAME = 'ers-runners-v1.5-fast'; // تحديث رقم النسخة
 const ASSETS_TO_CACHE = [
   './',
