fee250fa881f0f79edf0926e2f42b4cda6fbaa7a
ahmadwagiehlaw <AhmadWagieh@law.helwan.edu.eg>
2026-01-09 19:41:22 +0200

    Create env.js

---
+++ env.js
@@ -0,0 +1,8 @@
+// env.js
+const STRAVA_CONFIG = {
+    CLIENT_ID: "تجدها في Strava Dashboard",
+    CLIENT_SECRET: "8a4d1ed3b3f70f47aa7b90efce94a661dedfe2ba",
+    REFRESH_TOKEN: "f9b062e3252b8c3fc6c1c21af3eeb96cff9a44d4"
+};
+// خلي القيم دي متاحة عالمياً للتطبيق
+window.STRAVA_CONFIG = STRAVA_CONFIG;
